e30caeaa9f7d4bcaa23dd490b23d1a97fb18e306
Lesia Grapeniuk <lesiar29@ukr.net>
2025-06-07 15:47:05 +0300

    fixed header & added burger menu styles

--- src/app/shared/animations/global-animations.ts
+++ src/app/shared/animations/global-animations.ts
@@ -1,46 +1,70 @@
-import { trigger, state, style, transition, animate, query, stagger } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  query,
+  stagger,
+} from "@angular/animations";
 
-export const fadeExpand = trigger('fadeExpand', [
-  state('closed', style({
-    opacity: 0,
-    height: '0px',
-    padding: '0px',
-    overflow: 'hidden',
-  })),
-  state('open', style({
-    opacity: 1,
-    height: '*',
-    padding: '*',
-    overflow: 'hidden',
-  })),
-  transition('closed <=> open', animate('0.4s ease')),
+export const fadeExpand = trigger("fadeExpand", [
+  state(
+    "closed",
+    style({
+      opacity: 0,
+      height: "0px",
+      padding: "0px",
+      overflow: "hidden",
+    })
+  ),
+  state(
+    "open",
+    style({
+      opacity: 1,
+      height: "*",
+      padding: "*",
+      overflow: "hidden",
+    })
+  ),
+  transition("closed <=> open", animate("0.4s ease")),
 ]);
 
-export const fadeInOut = trigger('fadeInOut', [
-  transition(':enter', [
+export const fadeInOut = trigger("fadeInOut", [
+  transition(":enter", [
     style({ opacity: 0 }),
-    animate('500ms ease-in', style({ opacity: 1 })),
+    animate("500ms ease-in", style({ opacity: 1 })),
   ]),
-  transition(':leave', [
-    animate('400ms ease-out', style({ opacity: 0 })),
-  ]),
+  transition(":leave", [animate("400ms ease-out", style({ opacity: 0 }))]),
 ]);
 
-export const staggerFadeDown = trigger('staggerFadeDown', [
-  transition('out => in', [
+export const staggerFadeDown = trigger("staggerFadeDown", [
+  transition("out => in", [
     query(
-      '.advantages__content-block',
+      ".advantages__content-block",
       [
-        style({ opacity: 0, transform: 'translateY(-30px)' }),
+        style({ opacity: 0, transform: "translateY(-30px)" }),
         stagger(600, [
           animate(
-            '700ms ease-out',
-            style({ opacity: 1, transform: 'translateY(0)' })
-          )
-        ])
+            "700ms ease-out",
+            style({ opacity: 1, transform: "translateY(0)" })
+          ),
+        ]),
       ],
       { optional: true }
-    )
+    ),
   ]),
 ]);
 
+export const slideInOutRightToLeft = trigger("slideInOutRightToLeft", [
+  transition(":enter", [
+    style({ transform: "translateX(100%)", opacity: 0 }),
+    animate("400ms ease-in", style({ transform: "translateX(0)", opacity: 1 })),
+  ]),
+  transition(":leave", [
+    animate(
+      "400ms ease-in",
+      style({ transform: "translateX(-100%)", opacity: 0 })
+    ),
+  ]),
+]);
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,28 +1,41 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { SectionId } from '@shared/enums/section.enum';
-import { SectionsService } from '@shared/services/sections.service';
-import { UiKitModule } from '@shared/UI-KIT/ui-kit.module';
+import { CommonModule } from "@angular/common";
+import { Component, HostListener } from "@angular/core";
+import { TranslateModule } from "@ngx-translate/core";
+import {
+  fadeExpand,
+  fadeInOut,
+  slideInOutRightToLeft,
+} from "@shared/animations/global-animations";
+import { SectionId } from "@shared/enums/section.enum";
+import { SectionsService } from "@shared/services/sections.service";
+import { UiKitModule } from "@shared/UI-KIT/ui-kit.module";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   imports: [UiKitModule, CommonModule, TranslateModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.scss",
+  animations: [fadeExpand, fadeInOut, slideInOutRightToLeft],
 })
 export class HeaderComponent {
   public menuOpen = false;
+  public isScrolled = false;
   sectionId = SectionId;
 
   constructor(public sectionsService: SectionsService) {}
 
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 70;
+  }
   scrollTo(id: SectionId) {
     this.sectionsService.scrollToSection(id);
     this.menuOpen = false;
   }
-
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+  closeMenu(): void {
+    this.menuOpen = false;
   }
 }

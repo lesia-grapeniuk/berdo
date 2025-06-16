6c0f04191e21b2310bc241dbb0794ef78f241440
Lesia Grapeniuk <lesiar29@ukr.net>
2025-06-16 16:24:12 +0300

    Added 2 translation & videos

--- src/app/shared/data/route-data.ts
+++ src/app/shared/data/route-data.ts
@@ -2,9 +2,9 @@ import { RouteKey } from "@shared/enums/routeKeyTrackCarrousel.enams";
 
 
 export const ROUTE_VIDEO_MAP: Record<RouteKey, string> = {
-  [RouteKey.WALK]: 'assets/videos/video-main-muted.mp4',
-  [RouteKey.PANORAMAS]: 'assets/videos/video-main-muted.mp4',
-  [RouteKey.BRAVE]: 'assets/videos/video-main-muted.mp4',
-  [RouteKey.EXTREME]: 'assets/videos/video-main-muted.mp4',
-  [RouteKey.STUDY]: 'assets/videos/video-main-muted.mp4',
+  [RouteKey.WALK]: 'assets/videos/level-1.mp4',
+  [RouteKey.PANORAMAS]: 'assets/videos/panoramic.mp4',
+  [RouteKey.BRAVE]: 'assets/videos/hight-level.mp4',
+  [RouteKey.EXTREME]: 'assets/videos/extra.mp4',
+  [RouteKey.STUDY]: 'assets/videos/study.mp4',
 };

d88ab188ba41219efc79b3b13a3b517cb49bdbed
Lesia Grapeniuk <lesiar29@ukr.net>
2025-07-16 10:17:28 +0300

    returned routers path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,30 +3,56 @@ import { PolicePageComponent } from "@shared/components/police-page/police-page.
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'uk',
-    pathMatch: 'full',
+    path: "",
+    loadComponent: () =>
+      import("./features/dashboard/home-page/home-page.component").then(
+        (m) => m.HomePageComponent
+      ),
+    pathMatch: "full",
   },
   {
-    path: ':lang',
-    children: [
-      {
-        path: '',
-        loadComponent: () =>
-          import('./features/dashboard/home-page/home-page.component').then(
-            (m) => m.HomePageComponent
-          ),
-        pathMatch: 'full',
-      },
-      {
-        path: 'policy',
-        component: PolicePageComponent,
-      },
-      {
-        path: '**',
-        redirectTo: '',
-        pathMatch: 'full',
-      }
-    ],
+    path: "policy",
+    component: PolicePageComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "",
+    pathMatch: "full",
   },
 ];
+
+
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AppLanguage } from '@shared/enums/language.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+  private readonly defaultLang: AppLanguage = AppLanguage.Ukrainian;
+
+  constructor(private translate: TranslateService) {}
+
+  initLang(): void {
+    const savedLang = localStorage.getItem('appLang') as AppLanguage;
+
+    if (savedLang && Object.values(AppLanguage).includes(savedLang)) {
+      this.translate.setDefaultLang(savedLang);
+      this.translate.use(savedLang);
+    } else {
+      this.translate.setDefaultLang(this.defaultLang);
+      this.translate.use(this.defaultLang);
+      localStorage.setItem('appLang', this.defaultLang);
+    }
+  }
+
+  changeLang(lang: AppLanguage): void {
+    this.translate.use(lang);
+    localStorage.setItem('appLang', lang);
+  }
+
+  getCurrentLang(): string {
+    return this.translate.currentLang;
+  }
+}
--- src/app/shared/services/language.service.ts
+++ src/app/shared/services/language.service.ts
@@ -1,34 +1,33 @@
-import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Router } from '@angular/router';
-import { AppLanguage } from '@shared/enums/language.enum';
+import { Injectable } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { AppLanguage } from "@shared/enums/language.enum";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class LanguageService {
   private readonly defaultLang: AppLanguage = AppLanguage.Ukrainian;
-  private readonly supportedLangs = Object.values(AppLanguage);
 
-  constructor(
-    private translate: TranslateService,
-    private router: Router
-  ) {}
+  constructor(private translate: TranslateService) {}
 
   initLang(): void {
-    const urlLang = this.router.url.split('/')[1] as AppLanguage;
-    const langToUse = this.supportedLangs.includes(urlLang) ? urlLang : this.defaultLang;
+    const savedLang = localStorage.getItem("appLang") as AppLanguage;
 
-    this.translate.setDefaultLang(langToUse);
-    this.translate.use(langToUse);
+    if (savedLang && Object.values(AppLanguage).includes(savedLang)) {
+      this.translate.setDefaultLang(savedLang);
+      this.translate.use(savedLang);
+    } else {
+      this.translate.setDefaultLang(this.defaultLang);
+      this.translate.use(this.defaultLang);
+      localStorage.setItem("appLang", this.defaultLang);
+    }
   }
 
   changeLang(lang: AppLanguage): void {
     this.translate.use(lang);
-    this.router.navigate([`/${lang}`]);
   }
 
   getCurrentLang(): string {
-    return this.translate.currentLang || this.defaultLang;
+    return this.translate.currentLang;
   }
 }
